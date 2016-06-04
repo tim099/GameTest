1759e837dcf23305c0f8914b31adc029a8190a95
Tim <tim11251994@gmail.com>
2016-06-04 10:05:47 +0800

    openal test

--- src/AgeOfCube.cpp
+++ src/AgeOfCube.cpp
@@ -19,17 +19,41 @@
 #include "class/game/chessMaster/ChessMaster.h"
 
 #include "class/tim/pointer/smartPointerTest.h"
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
+ALuint buffer, source;
+void test_al(){
+    alutInit(0, NULL);
+    alGetError();
+
+    buffer = alutCreateBufferFromFile("files/wave/notitle.wav");//Notitle.wv
+    alGenSources(1, &source);
+	alSourcei(source,AL_BUFFER, buffer);
+    alSourcePlay(source);
+}
+void test_al_end(){
+	alSourcePause(source);
+    alDeleteSources(1, &source);
+    alDeleteBuffers(1, &buffer);
+    alutExit();
+}
 int main(){
-	//smart_pointer_test();
+
 
 
 	///*
 
 	//while(true){
+
+	test_al();
 	AOC::AgeOfCubes* ageOfCubes=new AOC::AgeOfCubes();
 	ageOfCubes->initialize();
 	ageOfCubes->mainloop();
 	delete ageOfCubes;
+	test_al_end();
+
+
 	//}
 
 	//*/
